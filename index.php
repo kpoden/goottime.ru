161df7c12741ff052a59ffdd3f1842f910a89c0a
Konstantin Podenezhniy <67436313+kpoden@users.noreply.github.com>
2023-08-31 22:41:44 +0500

    Готова начальная шапка и настроен scss

---
+++ index.php
@@ -0,0 +1,7 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+?>
+
+test 1345435435435 2332
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
---
+++ local/templates/goottime/header.php
@@ -0,0 +1,28 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+$CurDir = $APPLICATION->GetCurDir();
+$CurUri = $APPLICATION->GetCurUri();
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <?
+
+    use Bitrix\Main\Page\Asset;
+
+    // Пример подключения JS
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/jquery-3.7.1.min.js');
+//    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/fancybox/jquery.fancybox.js');
+//    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/myscripts.js');
+    // Пример подключения CSS
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/css/bootstrap-grid.min.css');
+//    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/css/vars.css');
+//    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/js/fancybox/jquery.fancybox.css');
+    $APPLICATION->ShowHead();
+    ?>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title><? $APPLICATION->ShowTitle() ?></title>
+</head>
+
+<div class="panel">
+    <? $APPLICATION->ShowPanel(); ?>
+</div>
